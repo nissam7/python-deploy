92a24f4a9abefdc3e86a1f23451605d6ba1f5c8c
Ubuntu <ubuntu@ip-172-31-4-254.eu-north-1.compute.internal>
2026-02-07 11:32:13 +0000

    788

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,42 @@
+import os
+import mysql.connector
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = db.cursor()
+
+# create table if not exists
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    message VARCHAR(255)
+)
+""")
+db.commit()
+
 @app.route('/submit', methods=['POST'])
 def submit():
     data = request.get_json()
-    name = data.get('name')
-    message = data.get('message')
+    name = data.get("name")
+    message = data.get("message")
 
-    print(f"Name: {name}, Message: {message}")
+    cursor.execute(
+        "INSERT INTO messages (name, message) VALUES (%s, %s)",
+        (name, message)
+    )
+    db.commit()
 
-    return jsonify({
-        "status": "Message received successfully!"
-    })
+    return jsonify({"status": "Saved to AWS RDS successfully!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
